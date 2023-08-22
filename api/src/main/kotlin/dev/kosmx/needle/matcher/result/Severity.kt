2a6327bf1c9f9d3f4e12a930fa5d94db4e54e225
KosmX <kosmx.mc@gmail.com>
2023-08-22 19:21:34 +0200

    Modify json format to keep compatibility

--- api/src/main/kotlin/dev/kosmx/needle/matcher/result/Severity.kt
+++ api/src/main/kotlin/dev/kosmx/needle/matcher/result/Severity.kt
@@ -1,28 +1,26 @@
 package dev.kosmx.needle.matcher.result
 
-import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonNames
 
 @Serializable
-@OptIn(ExperimentalSerializationApi::class)
 enum class Severity(val level: Int) {
 
     /**
      * It is very likely not a threat, but something may be an issue
      */
-    @JsonNames("INFO")
+    @SerialName("INFO")
     INFORMATION(0),
 
     /**
      * Threat isn't dangerous, something unwanted like data logging/etc, no token logging or backdoor access
      */
-    @JsonNames("POTENTIAL")
+    @SerialName("POTENTIAL")
     WARNING(1),
 
     /**
      * Serious threats like backdoor, token loggers, something that you definitely not want to run on your machine
      */
-    @JsonNames("MALWARE")
+    @SerialName("MALWARE")
     SEVERE(2),
 }
